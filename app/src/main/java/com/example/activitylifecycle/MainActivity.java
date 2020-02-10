c3d3968ca9e6f619a032c3bdec31cddecb4a7142
Asim Zahid <asimzahid02@gmail.com>
2020-02-10 15:02:45 +0500

    over write default life cycle methods and manually killed the activity on click.

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +15,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onstart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onPresume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "onstop", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void destroy(View v){
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 }
